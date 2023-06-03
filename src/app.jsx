d52ebcded077a3b885c1636eb96bffedba884c93
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-06-03 20:13:52 +0900

    transform js to jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-
-const Pet = (petInfo) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, petInfo.name),
-    React.createElement("h3", {}, petInfo.sex),
-    React.createElement("h3", {}, petInfo.breed),
-  ]);
-};
+import { createRoot } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "maru", sex: "male", breed: "dog" }),
-    React.createElement(Pet, { name: "capu", sex: "male", breed: "cat" }),
-    React.createElement(Pet, { name: "bbyorong", sex: "female", breed: "dog" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="maru" sex="male" breed="dog" />
+      <Pet name="capu" sex="male" breed="cat" />
+      <Pet name="bbyorong" sex="female" breed="dog" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
