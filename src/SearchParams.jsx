d3dd0cc61be214aae2f5a5c5b0e2279a68edc3cb
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-06-03 22:39:08 +0900

    introduce using map in jsx

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "raptile"];
 
 const SearchParams = () => {
   //const locationHook = useState("");
@@ -6,6 +7,10 @@ const SearchParams = () => {
   //const setLocation = locationHook[1];
   // above 3 line same as below
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form action="">
@@ -18,6 +23,40 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
