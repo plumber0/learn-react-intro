e1d1ad31a4f84aacd74fa9e2ca5f82b8eccf80c2
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-06-03 17:01:30 +0900

    set prettier and package.json

--- src/app.js
+++ src/app.js
@@ -1,26 +1,18 @@
 const Pet = (petInfo) => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h2", {}, petInfo.name),
-            React.createElement("h3", {}, petInfo.sex),
-            React.createElement("h3", {}, petInfo.breed),
-        ]
-    );
-}
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, petInfo.name),
+    React.createElement("h3", {}, petInfo.sex),
+    React.createElement("h3", {}, petInfo.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {name: "maru", sex: "male", breed:"dog"}),
-            React.createElement(Pet, {name: "capu", sex: "male", breed:"cat"}),
-            React.createElement(Pet, {name: "bbyorong", sex: "female", breed:"dog"}),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "maru", sex: "male", breed: "dog" }),
+    React.createElement(Pet, { name: "capu", sex: "male", breed: "cat" }),
+    React.createElement(Pet, { name: "bbyorong", sex: "female", breed: "dog" }),
+  ]);
 };
 
 const container = document.getElementById("root");
